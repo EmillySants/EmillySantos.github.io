7dd877a754d48fae3156aa321a28d74005a830b6
EmillySants <108280584+EmillySants@users.noreply.github.com>
2024-09-20 12:03:11 -0300

    aula1

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,6 +7,11 @@ async function vizualizarInformacoesGlobais() {
     paragrafo.classList.add('graficos-container__texto')
    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     console.log(paragrafo)
+    <p class="graficos-container_texto">
+Você sabia que o mundo tem 7888000000 de pessoas e que aproximadamente 5040000000 estão conectadas em alguma rede social e passam em média 2.38 horas conectadas."
+</p>
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
   }
 
 vizualizarInformacoesGlobais()
